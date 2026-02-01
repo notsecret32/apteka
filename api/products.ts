f745f1f8bd44578e6da57a94c4b09e29e26fb28f
notsecret32 <43242733+notsecret32@users.noreply.github.com>
2026-02-01 16:32:46 +0300

    feat: added cache for fetch

--- api/products.ts
+++ api/products.ts
@@ -1,7 +1,9 @@
 import { Product } from "@/lib/types";
 
 export async function getProducts(): Promise<Product[] | null> {
-  const data = await fetch("http://localhost:9080/api/products");
+  const data = await fetch("http://localhost:9080/api/products", {
+    cache: "force-cache",
+  });
 
   if (!data.ok) {
     return null;
